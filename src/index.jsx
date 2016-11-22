12d20bf6c8da955de310adad33fb3ee4a6f0a5b9
Synclair Wang <synclairwang11@yahoo.com>
2016-11-22 15:06:34 -0500

    working components

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React    from 'react';
 import ReactDOM from 'react-dom';
-// import App      from './components/App/App.jsx';
+import App      from './components/App/App.jsx';
 
-ReactDOM.render(<h1>Webpack working!!</h1>, document.querySelector('#root-container'));
+ReactDOM.render(<App />, document.querySelector('#root-container'));
