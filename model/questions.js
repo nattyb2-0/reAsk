dd94755d7aa592a7340c0e36ed6a2c2cf7acb1c6
mike cohen <mcohen113@msn.com>
2016-11-22 12:19:05 -0500

    model, showAllQuestions

--- model/questions.js
+++ model/questions.js
@@ -1,1 +1,14 @@
-questions.js
+const db = require('.db');
+
+function showAllQuestions(req, res, next) {
+  db.any('SELECT * FROM questions';)
+    .then(questions => {
+      res.questions = questions;
+      next();
+    })
+    .catch(error => next(error));
+}
+
+
+
+module.exports = { showAllQuestions }
