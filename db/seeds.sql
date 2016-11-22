66db3f4c2b1b61fe8e9bac6902b16c6bdcf9cf27
Synclair Wang <synclairwang11@yahoo.com>
2016-11-22 13:45:33 -0500

    edited schema and seeds file to include question title

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,13 +4,12 @@ INSERT INTO users (username, course, email, password, type) VALUES
 
   ('taka', 'WDI60Zonkeys', 'takataka@ga.edu', 'ahrens', 'student'),
   ('natty', 'WDI60Wholphins', 'nattyd@ga.edu', 'davis', 'student'),
-  ('trevor', 'WDI60Zonkeys', 'trevorp@ga.edu', 'teacher'
-    );
+  ('trevor', 'WDI60Zonkeys', 'trevorp@ga.edu', 'preston', 'teacher');
 
 INSERT INTO questions (username, votes, question_title, question_body, tag) VALUES
 
   ('taka', 3, 'question about position: relative', 'what is this shit?', 'css')
   ('natty', 6, 'What is react used for','Why can\'t i use something else other than react?', 'react');
 
-  COMMIT;
+COMMIT;
 
