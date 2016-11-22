ff38da9381c373f706bc5e58ffac54a5972b8ba0
Natty Davis <natty.davis79@gmail.com>
2016-11-22 13:33:14 -0500

    updated model function

--- models/users.js
+++ models/users.js
@@ -1,12 +1,28 @@
 
 //this function will query the database and  get all the users that are students from user table
 function showAllStudents(){
+  db.any('SELECT * from users WHERE ;')
+    .then((students) => {
 
+      res.students = students;
+      console.log(students);
+      next();
+    })
+    .catch(error => next(error));
+}
 }
 
 //this function will query the database and get all the users that are teachers
 function showAllteachers(){
+  db.any('SELECT * from users WHERE')
+    .then((teachers) => {
 
+      res.teachers = teachers;
+      console.log(teachers);
+      next();
+    })
+    .catch(error => next(error));
+}
 }
 
 module.exports ={showAllteachers,showAllStudents};
