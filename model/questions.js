8480f6c6f6a8c60ddebb938724ab4332408f8f73
mike cohen <mcohen113@msn.com>
2016-11-22 12:43:59 -0500

    model, upVoteQeustion

--- model/questions.js
+++ model/questions.js
@@ -1,6 +1,7 @@
 const db = require('.db');
 
 function showAllQuestions(req, res, next) {
+  console.log('stupid question')
   db.any('SELECT * FROM questions';)
     .then(questions => {
       res.questions = questions;
@@ -9,6 +10,27 @@ function showAllQuestions(req, res, next) {
     .catch(error => next(error));
 }
 
+function addQuestion(req, res, next) {
+db.one(`INSERT INTO questions (username, votes, question_body, tag)
+        VALUES ($username/, $/votes, $/question_body, $/tag)
+        RETURNING *;
+        `, req.body)
+  .then(questions => {
+    res.questions = questions;
+    next();
+  })
+  .catch(error => next(error));
+}
+
+function upVoteQuestion(req, res, next) {
+  db.none(`UPDATE questions
+           SET votes = votes + 1
+           WHERE id = ${req.params.id}`)
+          .then(() => {
+            next();
+          })
+          .catch(err => next(err))
+}
 
 
-module.exports = { showAllQuestions }
+module.exports = { showAllQuestions, addQuestion, upVoteQuestion };
