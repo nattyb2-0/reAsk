495ef349c8ccdea284230f946ab10d402e680104
Synclair Wang <synclairwang11@yahoo.com>
2016-11-22 16:53:06 -0500

    working on questions components scripts n shit

--- model/db.js
+++ model/db.js
@@ -1,11 +1,11 @@
 const pgp = require('pg-promise')();
 
 const config = process.env.DATABASE_URL || {
-  host:       process.env.DB_HOST,
-  port:       process.env.DB_PORT,
-  database:   process.env.DB_NAME,
-  user:       process.env.DB_USER,
-  password:   process.env.DB_PASS
+  host:       process.env.PG_HOST,
+  port:       process.env.PG_PORT,
+  database:   process.env.PG_DATABASE,
+  user:       process.env.PG_USER,
+  password:   process.env.PG_PASS
 };
 
 const db = pgp(config);
--- src/components/App/MainBody/QuestionsContainer/Questions/Questions.jsx
+++ src/components/App/MainBody/QuestionsContainer/Questions/Questions.jsx
@@ -4,12 +4,10 @@ import './Questions.css'
 class Questions extends React.Component {
   render() {
     return(
-      <div clasNames='questions'>
-        <div className='question'>
-          <h3>How do you make flexbox work?</h3>
-          <p>I kept trying over and over and shit just dont work fam</p>
-          <p>tags: HTML, CSS, Flexbox</p>
-        </div>
+      <div className='question'>
+        <h3>How do you make flexbox work?</h3>
+        <p>I kept trying over and over and shit just dont work fam</p>
+        <p>tags: HTML, CSS, Flexbox</p>
       </div>
     )
   }
