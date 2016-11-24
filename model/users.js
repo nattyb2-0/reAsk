2e347dfdd39920ca3f76aa4b625d07285a5de5de
Natty Davis <natty.davis79@gmail.com>
2016-11-24 10:35:08 -0500

    ready to commit

--- model/users.js
+++ model/users.js
@@ -57,28 +57,6 @@ function createUser(req, res, next) {
       res.redirect('/')
     }
   }
-// I commented this part part out taka...but im pretty sure you would have to do the insert in this way....
-// the one above was for mongo db where you didnt have to be specific about input and data andyou could toss anything at it.
-// function createUser(req, res, next) {
-//   if (req.body.user.password === req.body.user.confirmpassword) {
-//     const userObject = {
-//       username: req.body.user.username,
-//       email: req.body.user.email,
-//       course: req.body.user.course,
-//       password: req.body.user.password
-//     };
-//  getDB().then((db) => {
-//     db.one(`INSERT INTO users (username, course, email, password, type) VALUES ($1, $2, $3, $4, $5);`,
-//     [req.body.username, req.body.course, req.body.email, req.body.password, req.body.type])
-//   .then((users) => {
-//     res.users = users;
-//     db.close();
-//     next();
-//   });
-// })
-// }else {
-//   res.redirect('/')
-// }
-// }
+/
 
 module.exports = { showAllTeachers, showAllStudents, createUser };
