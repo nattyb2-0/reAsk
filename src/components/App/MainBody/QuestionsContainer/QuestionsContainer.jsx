3b1daad4526e456b830040b14dacde90f8b05acb
Jonathan Ahrens Ahrens <ahrens.jonathan@gmail.com>
2016-11-26 20:24:37 -0500

    added functionality to components and almost got render to work. Using static values at the moment

--- src/components/App/MainBody/QuestionsContainer/QuestionsContainer.jsx
+++ src/components/App/MainBody/QuestionsContainer/QuestionsContainer.jsx
@@ -3,36 +3,65 @@ import './QuestionsContainer.css'
 
 import Questions from './Questions/Questions.jsx'
 
-class QuestionsContainer extends React.Component {
+// class QuestionsContainer extends React.Component {
 
-  askQuestion() {
-    const askQuestionModal = document.querySelector('.ask-question-modal')
-    const coverThePage = document.querySelector('.cover-the-page')
+//   askQuestion() {
+//     const askQuestionModal = document.querySelector('.ask-question-modal')
+//     const coverThePage = document.querySelector('.cover-the-page')
 
-    askQuestionModal.style.display = 'block'
-    coverThePage.style.display = 'block'
-  }
+//     askQuestionModal.style.display = 'block'
+//     coverThePage.style.display = 'block'
+//   }
 
-  render() {
-    return(
-      <div className='questions-container'>
-        <button className='reAsk-button' onClick={this.askQuestion.bind(this)}>reAsk Something!</button>
-        <div className='questions'>
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-          <Questions />
-        </div>
-      </div>
+// // <Questions
+// //           key={i}
+// //           votes={q.votes}
+// //           question_title={q.question_title}
+// //           question_body={q.question_body}
+// //           question_tags={q.question_tags}
+// //         />
+//   // function takes the props of all questions
+//   renderQuestionList() {
+//     console.log('inside renderQuestionList, these are the props --> ', this.props);
+//     // this.props.getAllQuestions();
+//       // this.props.questions.map((q, i) => {
+//       this.props.questions.forEach((q, i) => {
+//         return (
+//           <h1>{i}</h1>
+//         );
+//       })
+//   }
+
+//   render() {
+//     return(
+//       <div className='questions-container'>
+//         <button className='reAsk-button' onClick={this.askQuestion.bind(this)}>reAsk Something!</button>
+//         <div className='questions'>
+//           {this.renderQuestionList()}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+
+const QuestionsContainer = props => {
+  console.log(props);
+  const questions = props.questions.map((q, i) => {
+    return (
+      <Questions
+
+      />
     )
-  }
+  });
+
+  return(
+    <div className='questions-container'>
+      <button className='reAsk-button' onClick={this.askQuestion.bind(this)}>reAsk Something!</button>
+      <div className='questions'>
+        {this.renderQuestionList()}
+      </div>
+    </div>
+  );
 }
 
 export default QuestionsContainer
